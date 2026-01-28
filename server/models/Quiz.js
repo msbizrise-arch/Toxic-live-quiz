3b27ee6fc2664e2965a8aa8b8c9a7c244bbe0a17
msbizrise-arch <msbizrise@gmail.com>
2026-01-28 13:31:45 +0530

    Update Quiz.js

--- server/models/Quiz.js
+++ server/models/Quiz.js
@@ -1,1 +1,9 @@
+const mongoose = require('mongoose');
 
+const QuizSchema = new mongoose.Schema({
+  testName: { type: String, unique: true },
+  timeLimit: { type: Number, default: 600 },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Quiz', QuizSchema);
