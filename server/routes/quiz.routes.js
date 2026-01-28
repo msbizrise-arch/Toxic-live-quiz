92a0bdfb40011be10d2e4657b064a991ed744768
msbizrise-arch <msbizrise@gmail.com>
2026-01-28 13:34:22 +0530

    Update quiz.routes.js

--- server/routes/quiz.routes.js
+++ server/routes/quiz.routes.js
@@ -1,1 +1,12 @@
+const router = require('express').Router();
+const Quiz = require('../models/Quiz');
 
+router.post('/create', async (req, res) => {
+  const { testName } = req.body;
+  await Quiz.create({ testName });
+  res.json({
+    link: `https://resonance-live-stream-xmtr.onrender.com/${testName}`
+  });
+});
+
+module.exports = router;
