2933df3d70210f74ca02742a12fa6772f4a2ec92
msbizrise-arch <msbizrise@gmail.com>
2026-01-28 13:33:26 +0530

    Update upload.routes.js

--- server/routes/upload.routes.js
+++ server/routes/upload.routes.js
@@ -1,1 +1,6 @@
+const router = require('express').Router();
+const upload = require('../middlewares/upload.middleware');
+const { uploadPDF } = require('../controllers/upload.controller');
 
+router.post('/', upload.single('pdf'), uploadPDF);
+module.exports = router;
