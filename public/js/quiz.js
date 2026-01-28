8cbd9939c64705f734302ebb202f95a19fb7f29c
msbizrise-arch <msbizrise@gmail.com>
2026-01-28 13:57:53 +0530

    Update quiz.js

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -1,1 +1,19 @@
+// Live Quiz logic - 1 question at a time
+const socket = io();
+const testName = location.pathname.replace("/", "");
+let username = "";
 
+function startQuiz() {
+    username = document.getElementById("name").value.trim();
+    if (!username) return alert("Enter your name");
+
+    socket.emit("join-quiz", testName);
+}
+
+socket.on("question", q => {
+    document.getElementById("question").innerText = q ? q.question : "Quiz Ended";
+});
+
+function nextQuestion() {
+    socket.emit("next-question", testName);
+}
